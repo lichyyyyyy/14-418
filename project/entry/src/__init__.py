aa7ab424c45a90585a2522c9879a72753f64b382
lichyyyyyy <lichy@ruc.edu.cn>
2021-10-03 10:42:25 -0400

    Build the main terminal application.

---
+++ project/entry/src/__init__.py
@@ -0,0 +1,10 @@
+def entry():
+    print("Welcome to Big Data Processing Application")
+    print("Please type the number that corresponds to which application you would like to run:")
+    print("1. Apache Hadoop")
+    print("2. Apache Spark")
+    print("3. Jupyter Notebook")
+    print("4. SonarQube and SonarScanner")
+
+    option = input("Type the number here > ")
+    print(option)
