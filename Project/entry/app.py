be6d606af1a87a7130078553a6ef8c58c0431717
lichyyyyyy <lichy@ruc.edu.cn>
2021-10-26 22:59:39 -0400

    entry application with simple ui

--- Project/entry/app.py
+++ Project/entry/app.py
@@ -1,14 +1,50 @@
-from flask import Flask, render_template
-import src
+from flask import Flask, redirect, render_template, request
+import os
 
-app = Flask(__name__, template_folder='src/templates')
+app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html')
+    return render_template('homepage.html')
+
+
+@app.route("/", methods=["POST"])
+def choose_application():
+    application = request.form['application']
+    print(application)
+    if application == '1':
+        return hadoop()
+    elif application == '2':
+        return spark()
+    elif application == '3':
+        return sonar()
+    return jupyter()
+
+
+@app.route("/hadoop")
+def hadoop():
+    hadoop_endpoint = os.environ.get("hadoop_endpoint")
+    return redirect(hadoop_endpoint)
+
+
+@app.route("/spark")
+def spark():
+    spark_endpoint = os.environ.get("spark_endpoint")
+    return redirect(spark_endpoint)
+
+
+@app.route("/sonar")
+def sonar():
+    sonar_endpoint = os.environ.get("sonar_endpoint")
+    return redirect(sonar_endpoint)
+
+
+@app.route("/jupyter")
+def jupyter():
+    jupyter_endpoint = os.environ.get("jupyter_endpoint")
+    return redirect(jupyter_endpoint)
 
 
 if __name__ == '__main__':
-    src.entry()
     app.run()
